ad8e3c3c54466be5be007250c88a8932993ca23b
Drew Whitney <drewlwhitney@gmail.com>
2025-11-01 17:18:33 -0600

    Made good progress moving to key overrides

--- keyboards/keebio/nyquist/rev4/keymaps/drew/custom_keys/alias.h
+++ keyboards/keebio/nyquist/rev4/keymaps/drew/custom_keys/alias.h
@@ -5,18 +5,17 @@
 
 #define TAB_FORWARD C(KC_TAB)
 #define TAB_BACKWARD (C(S(KC_TAB)))
+#define NUM_SPC LT(_NUM, KC_SPC)
+#define NAV_ENT LT(_NAV, KC_ENT)
 #define LAUNCH A(KC_SPC)
 #define MENU S(KC_F10)
-#define GUI_LAUNCH GUI_T(NK(TH_LAUNCH))
+#define GUI_LAUNCH GUI_T(LAUNCH)
 #define ALT_DEL ALT_T(NK(TH_DEL))
-#define ALT_COMP ALT_T(NK(TH_RALT))
 #define SFT_TILD SFT_T(NK(TH_TILD))
 #define SFT_SFT SFT_T(NK(TH_LSFT))
 #define SFT_COMM SFT_T(NK(TH_COMM))
 #define CTL_COLN CTL_T(NK(TH_COLN))
 
 // non-custom
-#define NUM_SPC LT(_NUM, TH_SPC)
-#define NAV_ENT LT(_NAV, KC_ENT)
 #define CAD C(A(KC_DEL))
 #define LOCK G(KC_L)
--- keyboards/keebio/nyquist/rev4/keymaps/drew/keymap.c
+++ keyboards/keebio/nyquist/rev4/keymaps/drew/keymap.c
@@ -8,8 +8,27 @@
 
 bool NAV_enabled = false;
 
+enum CustomKeycodes {
+    DOWN = SAFE_RANGE + 50,
+    UP,
+
+    LOW_6,
+    LOW_7,
+    LOW_8,
+    LOW_9,
+    LOW_0,
+};
+
 #define SFT_DEL SFT_T(KC_DEL)
 #define L_SFT SFT_T(KC_LSFT)
+#define ALT_COMP ALT_T(KC_RALT)
+
+/// @brief Neutralize flashing modifiers (Alt and Super) if they are active.
+static inline void neutralize_flashing_mods(void) {
+    if (get_mods() & MOD_MASK_AG) {
+        tap_code(DUMMY_MOD_NEUTRALIZER_KEYCODE);
+    }
+}
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -18,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,            KC_Q,                 KC_W,           KC_E,                  KC_R,            KC_T,               KC_Y,               KC_U,           KC_I,         KC_O,          KC_P,          KC_BSPC,
         CTL_T(KC_TAB),     KC_A,                 KC_S,           KC_D,                  KC_F,            KC_G,               KC_H,               KC_J,           KC_K,         KC_L,          KC_SCLN,       KC_QUOT,
         L_SFT,             KC_Z,                 KC_X,           KC_C,                  KC_V,            KC_B,               KC_N,               KC_M,           KC_COMM,      KC_DOT,        KC_SLSH,       SFT_DEL,
-        KC_RCTL,           LT(_FUNC, KC_MPLY),   ALT_T(KC_LALT), TD(TD_CAD_LOCK_SLEEP), GUI_LAUNCH,      NUM_SPC,            NAV_ENT,            ALT_COMP,       KC_LEFT,      NK(NK_DOWN),   NK(NK_UP),     KC_RGHT
+        KC_RCTL,           LT(_FUNC, KC_MPLY),   ALT_T(KC_LALT), TD(TD_CAD_LOCK_SLEEP), GUI_LAUNCH,      NUM_SPC,            NAV_ENT,            ALT_COMP,       KC_LEFT,      DOWN,          UP,            KC_RGHT
     ),
     // gamer
     [_GAME] = LAYOUT_ortho_4x12(
@@ -31,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // numbers
     [_NUM] = LAYOUT_ortho_4x12(
         KC_TRNS,           KC_SLSH,              KC_AT,          KC_ASTR,               NK(NK_MINS_NUM), KC_PLUS,            NK(NK_CIRC),        NK(NK_AMPR),    NK(NK_ASTR),  KC_LPRN,       NK(NK_RPRN),   KC_TRNS,
-        CTL_T(KC_DOT),     NK(NK_1),             NK(NK_2),       NK(NK_3),              NK(NK_4),        NK(NK_5),           NK(NK_6),           NK(NK_7),       NK(NK_8),     NK(NK_9),      NK(NK_0),      KC_TRNS,
-        SFT_COMM,          NK(NK_6_LOW),         NK(NK_7_LOW),   NK(NK_8_LOW),          NK(NK_9_LOW),    NK(NK_0_LOW),       KC_SCLN,            NK(NK_COLN),    KC_TRNS,      KC_TRNS,       KC_TRNS,       KC_TRNS,
+        CTL_T(KC_DOT),     KC_1,                 KC_2,           KC_3,                  KC_4,            KC_5,               KC_6,               KC_7,           KC_8,         KC_9,          KC_0,          KC_TRNS,
+        SFT_COMM,          LOW_6,                LOW_7,          LOW_8,                 LOW_9,           LOW_0,              KC_SCLN,            NK(NK_COLN),    KC_TRNS,      KC_TRNS,       KC_TRNS,       KC_TRNS,
         CTL_COLN,          KC_TRNS,              KC_TRNS,        KC_TRNS,               KC_TRNS,         KC_NO,              KC_TRNS,            KC_TRNS,        KC_TRNS,      KC_TRNS,       KC_TRNS,       KC_TRNS
     ),
     [_NAV] = LAYOUT_ortho_4x12(
@@ -60,32 +79,45 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
                     record->event.pressed ? layer_off(_NAV) : layer_on(_NAV);
                 }
             } else {
-                process_custom_key(NK(TH_SPC), record);
-                return false;
+                if (record->event.pressed) {
+                    uint8_t mods = get_mods();
+                    if (mods & MOD_MASK_CTRL) {
+                        del_mods(mods);
+                        tap_code16(MENU);
+                        set_mods(mods);
+                        return false;
+                    }
+                }
             }
             break;
+
         case NAV_ENT:
             if (!record->tap.count) {
                 NAV_enabled = record->event.pressed;
             }
             break;
+
+        case GUI_LAUNCH:
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(LAUNCH);
+                return false;
+            }
+            break;
+
         case SFT_DEL:
-            uint8_t mods = get_mods();
-            if (mods & MOD_MASK_GUI) {
-                if (record->tap.count) {
+            if (record->tap.count) { // only trigger on taps, not holds
+                uint8_t mods = get_mods();
+                if (mods & MOD_MASK_GUI) { // obliterate line forwards
                     if (record->event.pressed) {
                         del_mods(MOD_MASK_GUI);
                         tap_code16(S(KC_END));
                         register_code(KC_DEL);
                         set_mods(mods);
-                        tap_code(DUMMY_MOD_NEUTRALIZER_KEYCODE);
+                        return false;
                     } else {
-                        unregister_code(KC_DEL);
+                        neutralize_flashing_mods();
                     }
-                    return false;
-                }
-            } else if (get_mods() & MOD_MASK_SHIFT) {
-                if (record->tap.count) {
+                } else if (mods & MOD_MASK_SHIFT) { // caps word
                     if (record->event.pressed) {
                         caps_word_on();
                     }
@@ -94,13 +126,45 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
             }
             break;
 
+        case L_SFT:
+            static bool tab_backward_held = false;
+            if (record->tap.count) {
+                if (record->event.pressed) {
+                    uint8_t mods = get_mods();
+                    if (mods & MOD_MASK_ALT) {
+                        del_mods(MOD_MASK_ALT);
+                        register_code16(TAB_BACKWARD);
+                        set_mods(mods);
+                        tab_backward_held = true; // set the flag
+                        return false;
+                    } else if (mods & MOD_MASK_SHIFT) {
+                        caps_word_on();
+                        return false;
+                    }
+                } else {
+                    if (tab_backward_held) {
+                        tab_backward_held = false; // clear the flag
+                        unregister_code16(TAB_BACKWARD);
+                        neutralize_flashing_mods();
+                        return false;
+                    }
+                }
+            }
+            break;
+
+        case ALT_COMP:
+            // caps lock
+            if (record->tap.count && record->event.pressed && get_mods() & MOD_MASK_SHIFT) {
+                tap_code(KC_CAPS);
+                return false;
+            }
+            break;
+
         case CUSTOM_KEY_RANGE:
             process_custom_key(keycode, record);
             return false;
             break;
-        case GUI_LAUNCH:
         case ALT_DEL:
-        case ALT_COMP:
         case SFT_TILD:
         case SFT_SFT:
         case SFT_COMM:
@@ -151,7 +215,7 @@ bool turn_on_caps_word2(bool key_down, void* _) {
     return false;
 }
 
-const key_override_t Q_TO_KILL = ko_make_basic(MOD_MASK_GUI, KC_Q, A(KC_F4));
+const key_override_t Q_TO_KILL = ko_make_with_layers(MOD_MASK_GUI, KC_Q, A(KC_F4), _BASE + 1);
 
 const key_override_t BSPC_TO_OBLITERATE = {
     .trigger = KC_BSPC,
@@ -177,8 +241,55 @@ const key_override_t DEL_TO_CAPS_WORD = {
 
 const key_override_t ESC_TO_TAB_FORWARD = ko_make_basic(MOD_MASK_ALT, KC_ESC, TAB_FORWARD);
 
-// const key_override_t SFT_TO_TAB_BACKWARD =
-// ko_make_basic(MOD_MASK_ALT, L_SFT, );
+const key_override_t DOWN_TO_ZOOM_OUT = ko_make_basic(MOD_MASK_CTRL, DOWN, C(KC_MINS));
+const key_override_t DOWN_DEFAULT = ko_make_basic(0, DOWN, KC_DOWN);
+const key_override_t UP_TO_ZOOM_IN = ko_make_basic(MOD_MASK_CTRL, UP, C(KC_EQL));
+const key_override_t UP_DEFAULT = ko_make_basic(0, UP, KC_UP);
+
+const key_override_t ONE_TO_A = ko_make_basic(MOD_MASK_CTRL, KC_1, C(KC_A));
+const key_override_t ONE_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, KC_1, KC_1);
+
+const key_override_t TWO_TO_S = ko_make_basic(MOD_MASK_CTRL, KC_2, C(KC_S));
+const key_override_t TWO_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, KC_2, KC_2);
+
+const key_override_t THREE_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, KC_3, KC_3);
+
+const key_override_t FOUR_TO_F = ko_make_basic(MOD_MASK_CTRL, KC_4, C(KC_F));
+const key_override_t FOUR_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, KC_4, KC_4);
+
+const key_override_t FIVE_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, KC_5, KC_5);
+
+const key_override_t SIX_TO_H = ko_make_basic(MOD_MASK_CTRL, KC_6, C(KC_H));
+const key_override_t SIX_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, KC_6, KC_6);
+
+const key_override_t SEVEN_TO_J = ko_make_basic(MOD_MASK_CTRL, KC_7, C(KC_J));
+const key_override_t SEVEN_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, KC_7, KC_7);
+
+const key_override_t EIGHT_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, KC_8, KC_8);
+
+const key_override_t NINE_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, KC_9, KC_9);
+
+const key_override_t ZERO_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, KC_0, KC_0);
+
+const key_override_t LOW_6_TO_Z = ko_make_basic(MOD_MASK_CTRL, LOW_6, C(KC_Z));
+const key_override_t LOW_6_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, LOW_6, KC_6);
+const key_override_t LOW_6_DEFAULT = ko_make_basic(0, LOW_6, KC_6);
+
+const key_override_t LOW_7_TO_X = ko_make_basic(MOD_MASK_CTRL, LOW_7, C(KC_X));
+const key_override_t LOW_7_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, LOW_7, KC_7);
+const key_override_t LOW_7_DEFAULT = ko_make_basic(0, LOW_7, KC_7);
+
+const key_override_t LOW_8_TO_C = ko_make_basic(MOD_MASK_CTRL, LOW_8, C(KC_C));
+const key_override_t LOW_8_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, LOW_8, KC_8);
+const key_override_t LOW_8_DEFAULT = ko_make_basic(0, LOW_8, KC_8);
+
+const key_override_t LOW_9_TO_V = ko_make_basic(MOD_MASK_CTRL, LOW_9, C(KC_V));
+const key_override_t LOW_9_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, LOW_9, KC_9);
+const key_override_t LOW_9_DEFAULT = ko_make_basic(0, LOW_9, KC_9);
+
+const key_override_t LOW_0_TO_B = ko_make_basic(MOD_MASK_CTRL, LOW_0, C(KC_B));
+const key_override_t LOW_0_NOSHIFT = ko_make_basic(MOD_MASK_SHIFT, LOW_0, KC_0);
+const key_override_t LOW_0_DEFAULT = ko_make_basic(0, LOW_0, KC_0);
 
 // this globally defines all key overrides to be used
 const key_override_t* key_overrides[] = {
@@ -187,4 +298,40 @@ const key_override_t* key_overrides[] = {
     // &DEL_TO_OBLITERATE,
     &DEL_TO_CAPS_WORD,
     &ESC_TO_TAB_FORWARD,
+
+    &DOWN_TO_ZOOM_OUT,
+    &DOWN_DEFAULT,
+    &UP_TO_ZOOM_IN,
+    &UP_DEFAULT,
+
+    &ONE_TO_A,
+    &ONE_NOSHIFT,
+    &TWO_TO_S,
+    &TWO_NOSHIFT,
+    &THREE_NOSHIFT,
+    &FOUR_TO_F,
+    &FOUR_NOSHIFT,
+    &FIVE_NOSHIFT,
+    &SIX_TO_H,
+    &SIX_NOSHIFT,
+    &SEVEN_TO_J,
+    &SEVEN_NOSHIFT,
+    &EIGHT_NOSHIFT,
+    &NINE_NOSHIFT,
+    &ZERO_NOSHIFT,
+    &LOW_6_TO_Z,
+    &LOW_6_NOSHIFT,
+    &LOW_6_DEFAULT,
+    &LOW_7_TO_X,
+    &LOW_7_NOSHIFT,
+    &LOW_7_DEFAULT,
+    &LOW_8_TO_C,
+    &LOW_8_NOSHIFT,
+    &LOW_8_DEFAULT,
+    &LOW_9_TO_V,
+    &LOW_9_NOSHIFT,
+    &LOW_9_DEFAULT,
+    &LOW_0_TO_B,
+    &LOW_0_NOSHIFT,
+    &LOW_0_DEFAULT,
 };
