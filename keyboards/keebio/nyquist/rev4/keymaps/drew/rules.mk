d3d045af444f7736b7d3582dd1b0ff1e33f91a32
Drew Whitney <drewlwhitney@gmail.com>
2025-11-04 00:22:14 -0700

    Disabled flow tap, it doesn't seem necessary

--- keyboards/keebio/nyquist/rev4/keymaps/drew/rules.mk
+++ keyboards/keebio/nyquist/rev4/keymaps/drew/rules.mk
@@ -2,7 +2,7 @@ SRC += caps_word/caps_word.c
 SRC += taphold/taphold_options.c
 SRC += utils/utils.c
 # SRC += chordal_hold/chordal_options.c
-SRC += flow_tap/flow_tap_options.c
+# SRC += flow_tap/flow_tap_options.c
 
 # VIA_ENABLE = yes
 LTO_ENABLE = yes
@@ -13,3 +13,4 @@ CAPS_WORD_ENABLE = yes
 
 TAP_DANCE_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
+
