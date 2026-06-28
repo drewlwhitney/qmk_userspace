366bde3c8de581574ac804e48a36f79ed7dd65a8
Drew Whitney <drewlwhitney@gmail.com>
2026-06-28 16:48:50 -0600

    Fixed a minor issue

--- keyboards/keebio/iris_lm/keymaps/drew/keymap.c
+++ keyboards/keebio/iris_lm/keymaps/drew/keymap.c
@@ -134,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //├───────────┼───────────┼───────────┼───────────┼───────────┼───────────┤                           ├───────────┼───────────┼───────────┼───────────┼───────────┼───────────┤
    KC_TRNS,    KC_SCLN,    KC_COLN,    KC_UNDS,    KC_MINS,    KC_EQL,                                 KC_DQUO,    OS_LSFT,    CTL_BSLS,   ALT_PIPE,   OS_LGUI,    KC_TRNS,
 //├───────────┼───────────┼───────────┼───────────┼───────────┼───────────┼───────────┐   ┌───────────┼───────────┼───────────┼───────────┼───────────┼───────────┼───────────┤
-   KC_TRNS,    KC_LPRN,    KC_RPRN,    KC_LBRC,    KC_RBRC,    KC_RABK,    KC_TRNS,        KC_TRNS,    KC_PLUS,    KC_BSPC,    KC_LCBR,    KC_RCBR,    KC_GRV,     KC_RABK,
+   KC_TRNS,    KC_LPRN,    KC_RPRN,    KC_LBRC,    KC_RBRC,    KC_RABK,    KC_TRNS,        KC_TRNS,    KC_PLUS,    KC_BSPC,    KC_LCBR,    KC_RCBR,    KC_TILDE,     KC_RABK,
 //└───────────┴───────────┴───────────┴─────┬─────┴─────┬─────┴─────┬─────┴────┬──────┘   └─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴───────────┴───────────┴───────────┘
                                              KC_TRNS,    KC_TRNS,    KC_TRNS,                    KC_TRNS,    KC_TRNS,    KC_TRNS
 //                                          └───────────┴───────────┴──────────┘                └───────────┴───────────┴───────────┘
